4e6cad788e9acf3996c89e808df87b0158cc41e6
권상현 sang kwon <sangcode33@gmail.com>
2022-03-08 21:52:09 +0900

    K번째수
    1. array를 slice를 해주고 sort를 해주고 
    2. commands의 두번째 인덱스 값을 구하고 answer에 push 한 후 리턴

---
+++ 0308.js
@@ -0,0 +1,9 @@
+https://programmers.co.kr/learn/courses/30/lessons/42748
+
+function solution(array, commands) {
+    let answer = [];
+    for (let i in commands) { 
+        answer.push(array.slice(commands[i][0]-1, commands[i][1]).sort((a,b)=>a-b)[commands[i][2]-1])
+    }
+    return answer;
+}
