27e944ff5b0fea28ddd654523559e0082c0a155a
권상현 sang kwon <sangcode33@gmail.com>
2022-03-11 23:50:02 +0900

    같은 숫자는 싫어
    1. 반복되는 숫자가 arr[i] 다음 숫자이니 그에 맞는 조건문을 걸어주고 answer에 push해서 리턴

---
+++ 0311.js
@@ -0,0 +1,11 @@
+https://programmers.co.kr/learn/courses/30/lessons/12906
+
+function solution(arr) {
+    let answer = [];
+    for (let i = 0; i < arr.length; i++) { 
+        if (arr[i] !== arr[i + 1]) { 
+            answer.push(arr[i]);
+        }
+    }
+    return answer;
+}
