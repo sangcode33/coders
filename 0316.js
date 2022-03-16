7ce9dad1e49e8c231887a28fc74412db12862465
권상현 sang kwon <sangcode33@gmail.com>
2022-03-16 23:35:32 +0900

    짝수와 홀수
    1. 2로 나누었을 때 나머지가 0이면 Even을 리턴 
    2. 아닐 경우 Odd를 리턴

---
+++ 0316.js
@@ -0,0 +1,11 @@
+https://programmers.co.kr/learn/courses/30/lessons/12937
+
+function solution(num) {
+    let answer = '';
+    if (num % 2 === 0) {
+        return answer = "Even"
+    } else {
+        return answer = "Odd"
+    }
+    return answer;
+}
