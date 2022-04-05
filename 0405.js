acb6615ac3922c5ff19a7a642de1d273f7f11d96
권상현 sang kwon <sangcode33@gmail.com>
2022-04-05 22:38:52 +0900

    124 나라의 숫자
    1. ['4', '1', '2']인 배열을 선언하고 (4, 1, 2인 이유는 3으로 나눈 경우 나머지가 0, 1, 2임에 따라 4, 1, 2가 나와야 하기 때문)
    2. 매개변수 n이 0보다 클 때까지 계속 3으로 나눠주며 그에 따른 결과값들을 계속 더해주어 124나라 진수값에 맞는 문자열을 answer변수에 할당한다
    3. 최종적으로 할당된 answer 값을 리턴

---
+++ 0405.js
@@ -0,0 +1,11 @@
+https://programmers.co.kr/learn/courses/30/lessons/12899
+
+function solution(n) {
+    let answer = '';
+    const arr = ['4', '1', '2'];
+    while(n > 0) {
+        answer = arr[n % 3] + answer;
+        n = Math.floor((n - 1) / 3);
+    }
+    return answer;
+}
