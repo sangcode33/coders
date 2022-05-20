c0b0698e9652014001ad7f06827526356a9bed94
권상현 sang kwon <sangcode33@gmail.com>
2022-05-20 19:02:27 +0900

    다리를 지나는 트럭
    1. 배열 bridge의 length가 bridge_length와 같으면 배열 bridge 맨 앞 값을 shift 메소드로 제거
    2. bridge_weight와 truck_weights[0]의 합이 weight 보다 크면 push 메소드로 0을 추가하고
    3. 그렇지 ㅇ낳다면 push 메소드로 truck_weight을 추가
    4. anwer 리턴

---
+++ 0520.js
@@ -0,0 +1,22 @@
+https://programmers.co.kr/learn/courses/30/lessons/42583
+
+function solution(bridge_length, weight, truck_weights) {
+    let answer = 0;
+    let bridge = [];
+    let bridge_weight = 0; 
+    while (truck_weights.length > 0) {
+        answer++;
+        if (bridge.length === bridge_length) {
+            bridge_weight = bridge_weight - bridge.shift();
+        }
+        if (bridge_weight + truck_weights[0] > weight) {
+            bridge.push(0);
+            continue;
+        }
+        let truck_weight = truck_weights.shift();
+        bridge.push(truck_weight);
+        bridge_weight = bridge_weight + truck_weight;
+    }
+    answer = answer + bridge_length;
+    return answer;
+}
