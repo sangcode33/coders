4a92372f3f647c07f36114e452023b8a89d98139
권상현 sang kwon <sangcode33@gmail.com>
2022-05-24 23:11:31 +0900

    이진 변환 반복하기
    1. 매개변수 s가 1이 될 때까지 이진 변환을 해주고 
    2. 배열 answer에 모든 0의 개수를 담아주고 리턴

---
+++ 0524.js
@@ -0,0 +1,11 @@
+https://programmers.co.kr/learn/courses/30/lessons/70129
+
+function solution(s) {
+    let answer = [0,0];
+    while (s.length > 1) {
+        answer[0]++;
+        answer[1] = answer[1] + (s.match(/0/g) || []).length;
+        s = s.replace(/0/g, '').length.toString(2);
+    }
+    return answer;
+}
